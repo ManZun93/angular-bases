7c3a68f2ba575872c20e732a839d910b488c06a8
ManZun93 <manuellufan19@gmail.com>
2023-09-06 20:48:47 -0500

    servicio privado

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'dbz-add-character',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-character.component.css']
 })
 export class AddCharacterComponent {
+
+  @Output() //decorador para emitir eventos al padre(Main-Page)
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter(); //EventEmitter es un generico <> para emitir eventos
+
+  public character: Character = {
+    id: uuid(),
+    name: '',
+    power: 0,
+  };
+
+  emitCharacter():void {
+    console.log(this.character);
+
+    if(this.character.name.length === 0 ) return;
+
+    this.onNewCharacter.emit(this.character); //aqui utilizamos nuestro emisor y le damos salida al evento
+
+    this.character = {name:'', power: 0, id:uuid()};
+  };
+
+
+
 
 }
--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter,Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'dbz-list',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+@Input() //decorador Input sirve para recibir propiedades del padre
+public characterList: Character[] = [{
+  //id: uuid(),
+  name: 'Trunks',
+  power: 10,
+}]
+
+@Output()
+public onDelete:EventEmitter<string> = new EventEmitter()
+
+onDeleteCharacter(id?:string):void {
+
+console.log({id})
+// this.characterList.splice(index)
+if (!id) return;
+this.onDelete.emit(id)
+
 }
+
+
+
+
+};
+
+
+
